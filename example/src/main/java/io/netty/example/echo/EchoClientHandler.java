2ba25331e649acc89f342a6490ccac1e5ad9c79c
Ash-Shang <ash159@qq.com>
2021-04-11 07:53:17 +0800

    netty echo用例

--- example/src/main/java/io/netty/example/echo/EchoClientHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoClientHandler.java
@@ -35,7 +35,8 @@ public class EchoClientHandler extends ChannelInboundHandlerAdapter {
     public EchoClientHandler() {
         firstMessage = Unpooled.buffer(EchoClient.SIZE);
         for (int i = 0; i < firstMessage.capacity(); i ++) {
-            firstMessage.writeByte((byte) i);
+            String test = "demo" + i;
+            firstMessage.writeBytes(test.getBytes());
         }
     }
 
